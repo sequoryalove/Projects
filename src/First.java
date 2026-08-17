947c39adafe365bb99089d52f484b8e3c2ea05be
sequorya love <sequoryal@MacBookAir.home.local>
2026-08-16 21:29:51 -0500

    Initial commit

---
+++ src/First.java
@@ -0,0 +1,9 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Information Security Analyst");
+        System.out.println("Why: I want to protect computer systems and networks from cyber threats.");
+        System.out.println("Median salary: $124,910");
+    }
+
+}
