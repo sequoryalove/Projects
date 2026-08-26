c4fe54b7db16fdb77ac488cb55896d7c336a29e0
sequorya love <sequoryal@sequoryas-MacBook-Air.local>
2026-08-25 21:44:40 -0500

    Add Tests program

---
+++ src/Tests.java
@@ -0,0 +1,12 @@
+public class Tests {
+    public static void main(String[] args) {
+        double testScore1 = 88.2;
+        double testScore2 = 78.9;
+        double testScore3 = 97.6;
+        double average = (testScore1 + testScore2 + testScore3) / 3;
+        System.out.println("Test score 1: " + testScore1);
+        System.out.println("Test score 2: " + testScore2);
+        System.out.println("Test score 3: " + testScore3);
+        System.out.println("The average of 3 test scores is: " + average);
+    }
+}
